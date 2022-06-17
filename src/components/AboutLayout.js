28abaf792a65dbed55b36365bbb9433caf21f912
Akhil <108akhilraj@gmail.com>
2022-06-17 12:27:01 +0530

    sort by feature added

--- src/components/AboutLayout.js
+++ src/components/AboutLayout.js
@@ -3,9 +3,25 @@ import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box'
+import Link from '@mui/material/Link';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Ecom
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 
 const AbouLayout = () => {
+  
   return (
     <>
       <CssBaseline />
@@ -16,13 +32,15 @@ const AbouLayout = () => {
         
           <div style={{display:"flex", justifyContent:"center"}}>
               <Box sx={{width: 500, height: 300}}>
-              <Typography mt={2} variant='h4' padding={0} align="center"> 
+              <Typography mt={2} variant='h4' padding={0} align="center" > 
                 We are on a mission to make 
                 Quality products more accessible
               </Typography>
               </Box>
           </div>
-      </Container> 
+      </Container>       
+        <Copyright sx={{ mt: 5 }} />
+
     </>
   )
 }
--- src/components/ProductLayout.js
+++ src/components/ProductLayout.js
@@ -1,23 +1,78 @@
-import React from 'react';
+import React, {useState } from 'react';
 import Typography from '@mui/material/Typography';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 import Products from "./Products/Products";
 
+
 const ProductLayout = ({products,onAddToCart}) => {
+
+  const [sort,setSort]=useState("");
+
+  const handleChange = (event) => {
+    setSort(event.target.value);
+    if(event.target.value==="plh"){
+      products.sort((a,b)=>{
+        if(a.price.raw===b.price.raw){
+          return a.name-b.name
+        }
+        return a.price.raw-b.price.raw
+      })
+    }
+    if(event.target.value==="phl"){
+      products.sort((a,b)=>{
+        if(a.price.raw===b.price.raw){
+          return a.name-b.name
+        }
+        return b.price.raw-a.price.raw
+      })
+    }
+    if(event.target.value==="none"){
+      products.sort((a,b)=>{
+        return a.created-b.created;
+      })
+    }
+    
+  
+  };
+
   if(!products.length){
     return(
       <div style={{display:"flex", justifyContent:"center"}}>
           <Typography mt={20} variant='h4'>
-          In a moment...
+          Loading the products...
           </Typography>
       </div>
   )
   }
 
-
   return (
-        <div style={{margin:"30px"}}>
+       <>
+        <div>
+            <FormControl sx={{marginTop:"10px",padding:"20px"}}>
+          <FormLabel id="demo-controlled-radio-buttons-group">Sort By</FormLabel>
+          <RadioGroup
+             row
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={sort}
+            onChange={handleChange}
+          >
+            <FormControlLabel value="none" control={<Radio color="success"/>} label="None" />
+            <FormControlLabel value="plh" control={<Radio color="success"/>} label="price low to high" />
+            <FormControlLabel value="phl" control={<Radio color="success"/>} label="price high to low" />
+          </RadioGroup>
+        </FormControl>
+        </div>
+
+         <div style={{margin:"30px"}}>
             <Products products={products} onAddToCart={onAddToCart}/>
         </div>
+
+      </>
   )
 }
 
