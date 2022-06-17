d534e4f9c0e26e700fefb8a078d8b32ad51bdb76
Akhil <108akhilraj@gmail.com>
2022-06-17 10:43:55 +0530

    category wise products listing

--- src/components/Category/CategoryItem.js
+++ src/components/Category/CategoryItem.js
@@ -5,11 +5,15 @@ import ProductLayout from '../ProductLayout';
 const CategoryItem = ({pro,onAddToCart}) => {
 
     const [items,setItems]=useState([]);
-
+    
     useEffect(()=>{
-        const {data}=commerce.categories.retrieve('category', { type: `${pro}` })
-        setItems(data);
+        async function fetchData(){
+          const {data}=await commerce.products.list({category_slug: [`${pro}`]})
+          setItems(data)
+        }
+        fetchData();
     },[pro])
+
 
   return (
     <ProductLayout products={items} onAddToCart={onAddToCart}/>
--- src/components/ProductLayout.js
+++ src/components/ProductLayout.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
 import Products from "./Products/Products";
 
 const ProductLayout = ({products,onAddToCart}) => {
+  if(!products.length){
+    return(
+      <div style={{display:"flex", justifyContent:"center"}}>
+          <Typography mt={20} variant='h4'>
+          In a moment...
+          </Typography>
+      </div>
+  )
+  }
+
+
   return (
         <div style={{margin:"30px"}}>
             <Products products={products} onAddToCart={onAddToCart}/>
